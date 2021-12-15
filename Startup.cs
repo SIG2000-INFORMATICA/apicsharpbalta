7fda2c69ba2b35b72058691af8313d7291e68979
Tiago Andrade <87506421+T-Andrade33@users.noreply.github.com>
2021-12-15 11:09:18 -0300

    Ajustes 3 - Adicionado o Cors e politica para autenticação de SSL

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.Certificate;
 using Shop.Data;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
@@ -30,10 +31,18 @@ namespace Shop
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
             services.AddControllers();
             var connectionString = Configuration["ConnectionStrings : BaseConnection"];        
             services.AddDbContext<DataContext>(options => options.UseNpgsql(Configuration.GetConnectionString("BaseConnection")));
             services.AddScoped<DataContext, DataContext>();
+            services.AddCors(options => 
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +52,8 @@ namespace Shop
             {
             app.UseDeveloperExceptionPage();
             }
+            app.UseAuthentication();
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
